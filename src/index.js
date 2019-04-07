fde88fbada5c56ccc24209f6a48bb47a9b919163
Piotr <ptrysinski@gmail.com>
2019-04-08 00:37:46 +0200

    step 3

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={function() { alert('click'); }}>
+          {this.props.value}
         </button>
       );
     }
